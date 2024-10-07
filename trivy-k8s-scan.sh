2e67d043d7c9541233285476a079a3bc3d3a51e4
Kanat121 <140329988+Kanat121@users.noreply.github.com>
2024-10-07 10:52:15 +0200

    Update trivy-k8s-scan.sh

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -13,7 +13,7 @@ docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.55.2 -q image --exit
     # Check scan results
     if [[ ${exit_code} == 1 ]]; then
         echo "Image scanning failed. Vulnerabilities found"
-        # exit 1;
+        # exit 1; если раскоментировать здесь, при обнаружение критических уязвимости пайплайн остановиться 
     else
         echo "Image scanning passed. No vulnerabilities found"
     fi;
